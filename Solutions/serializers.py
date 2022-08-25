2fba4214d7796320ae49cd7443eb505c4b18a6f3
bano <bbannoooo@gmail.com>
2022-08-25 22:51:03 +0900

    Update Solutions models

--- Solutions/serializers.py
+++ Solutions/serializers.py
@@ -2,6 +2,7 @@ from Solutions.models import Solutions
 from rest_framework import serializers
 
 class SolutionsSerializer(serializers.ModelSerializer):
+    pesticide_image = serializers.ImageField(required=False, use_url=True)
     class Meta:
         model = Solutions
         fields = '__all__'
