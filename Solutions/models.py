fd1cde890122cfeb8d472cf80e8324bcb2d7ae47
bano <bbannoooo@gmail.com>
2022-08-26 15:40:52 +0900

    Modify Solutions model

--- Solutions/models.py
+++ Solutions/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Solutions(models.Model):
     solution_id = models.IntegerField()
     solution_default = models.TextField()
-
+    # pesticide = models.CharField(max_length=100)
+    # pesticide_image = models.ImageField(upload_to='pesticide_image', default='media/default-image.jpg', null=True)
+    # pesticide_link = models.URLField()
     class Meta:
         db_table = 'solutions'
